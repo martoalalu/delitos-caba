4012d5fdc3c71f2285873183ca6ba2c555438916
martoalalu <martinalalu@gmail.com>
2019-12-17 17:56:58 -0300

    test i moran y regresion

--- tp_delitos.R
+++ tp_delitos.R
@@ -9,8 +9,12 @@ library(data.table)
 library(ggmap)
 library(janitor)
 library(ggdark)
+library(leaflet)
 
 delitos <- fread("https://raw.githubusercontent.com/martoalalu/delitos-caba/master/data/delitos.csv")
+delitos <- delitos %>% 
+  mutate(anio=year(fecha))
+
 delitos_geo <- delitos %>% 
   filter(!is.na(longitud), !is.na(latitud)) %>%
   filter(longitud != 0) %>% 
@@ -19,8 +23,7 @@ delitos_geo <- delitos %>%
 colnames(delitos_geo)
 unique(delitos_geo$tipo_delito)
 
-delitos_geo_robo <- delitos_geo %>% 
-  filter(tipo_delito =="Robo (Con violencia)")
+
 
 bbox <- c(-58.546686, #Asignar coordenadas de los límites del bounding box de la Ciudad
           -34.711145,
@@ -44,6 +47,7 @@ ggmap(CABA) +
   scale_fill_distiller(palette = "Spectral")+
   theme_void()
 
+#Delitos facetados por anio
 ggmap(CABA) +
   stat_density_2d(data = delitos, 
                   aes(x = longitud, y = latitud, 
@@ -56,7 +60,7 @@ ggmap(CABA) +
        fill="Nivel")+
   scale_fill_distiller(palette = "Spectral")+
   theme_void()+
-  facet_wrap(~tipo_delito)
+  facet_wrap(~anio)
 
 radios <- read_sf("http://cdn.buenosaires.gob.ar/datosabiertos/datasets/informacion-censal-por-radio/caba_radios_censales.geojson")
 
@@ -96,12 +100,12 @@ ggplot() +
   scale_fill_viridis_c(alpha = 0.9)
 
 
-calles2 <- read_sf("C:/Users/20332842324/Desktop/callejero.shp")
+calles <- read_sf("C:/Users/20332842324/Desktop/callejero.shp")
 
 #Paso a misma proyección que delitos
-calles <- st_transform(calles2,crs = 4326)
+calles <- st_transform(calles,crs = 4326)
 
-#Nos quedamos con los delitos de caba
+#Nos quedamos con los delitos que están dentro de CABA
 delitos_caba <- delitos_geo[st_within(delitos_geo, barrios) %>% lengths > 0,]
 
 #Chequeo coordenadas
@@ -140,12 +144,25 @@ calles <- calles %>%
   mutate(cant_delitos_log=log(cant_delitos))
 
 ggplot()+
+  geom_histogram(data=calles,aes(x=cant_delitos_log))
+
+
+ggplot()+
   geom_sf(data = calles, aes(color=cant_delitos_log), size=0.7)+
   labs(title = "Cantidad de delitos por calle",
        subtitle="Ciudad Autónoma de Buenos Aires",
        caption= "Fuente de datos: https://mapa.seguridadciudad.gob.ar/")+
   scale_colour_viridis_c(option="inferno",guide=FALSE)+
   dark_theme_void()
+
+binpal <- colorBin("viridis", calles$cant_delitos_log, 6, pretty = FALSE)
+
+leaflet(calles) %>% 
+  addProviderTiles(providers$CartoDB.DarkMatter) %>% 
+  addPolylines(color = ~binpal(cant_delitos_log), weight = 1.5, smoothFactor = 0.5, 
+               opacity = 1.0, fillOpacity = 0.65,highlightOptions = highlightOptions(color = "white", weight = 2,
+                bringToFront = TRUE))
+
 
 ggplot()+
   geom_histogram(data=calles,aes(x=cant_delitos_log))
@@ -173,11 +190,87 @@ ggplot(data=calles_delitos_q_unique[1:30,],
        y = "Cantidad")+
   guides(fill=FALSE)
 
+View(head(delitos_caba))
 
-geom_text(aes(label = y), position = position_nudge(y = -0.1))
+
+delitos_anio <- delitos %>% 
+  count(anio)
+
+delitos_anio <- delitos_anio %>% 
+  mutate(pct=n/sum(delitos_anio$n))
+
+delitos_anio
 
 
-View(head(calles))
+
+
+
+
+
+# TEST I DE MORAN
+
+caba <- st_read("http://cdn.buenosaires.gob.ar/datosabiertos/datasets/perimetro/caba-perimetro.geojson")
+
+#Creamos grilla
+grilla<-st_make_grid(caba, cellsize = 0.003)
+
+
+#Vemos como queda
+ggplot() + 
+  geom_sf(data = caba, fill=NA)+
+  geom_sf(data=grilla, fill=NA)
+
+#Convertimos la grilla a objeto sf
+grilla <- st_sf(grilla)
+grilla <- mutate(grilla, id=seq(1,3960))
+
+grilla_caba <- st_intersection(grilla, caba)
+
+delitos_robo <- delitos_caba %>% 
+  filter(tipo_delito =="Robo (Con violencia)")
+
+ggplot()+
+  geom_sf(data=grilla_caba)+
+  geom_sf(data=delitos_robo)
+
+delitos_robo <- delitos_robo %>%  
+  st_join(grilla_caba)
+
+delitos_robo_grilla <- delitos_robo %>% 
+  st_set_geometry(NULL) %>% 
+  group_by(id.y) %>% 
+  summarise(robos = n())
+
+
+ggplot() +
+  geom_histogram(data = delitos_robo_grilla, aes(robos))
+
+grilla_caba <- grilla_caba %>% 
+  left_join(delitos_robo_grilla, by=c("id"="id.y"))
+
+grilla_caba <- grilla_caba %>% 
+  mutate(robo = ifelse(is.na(robos), 0, robos))
+
+plot(grilla_caba["mueblerias"], border = NA)
+
+ggplot()+
+  geom_sf(data=grilla_caba, aes(fill=robo))
+
+
+library(spdep)
+grilla_caba <- st_as_sf(grilla_caba)
+
+sp.matrix <- poly2nb(as(grilla_caba, "Spatial"), queen=TRUE)
+summary(sp.matrix)
+sp.weights <- nb2listw(sp.matrix, style='W')
+
+moran.mc(grilla_caba$robo, listw = sp.weights, nsim = 999)
+
+#Distancia a comisarias
+grilla_caba_c <- st_point_on_surface(grilla_caba)
+
+ggplot()+
+  geom_sf(data=grilla_caba_c, aes(color=robo))
 
 comisarias<-read.csv("http://cdn.buenosaires.gob.ar/datosabiertos/datasets/comisarias-policia-de-la-ciudad/comisarias-policia-de-la-ciudad.csv")
 
@@ -185,6 +278,20 @@ comisarias <- comisarias %>%
   filter(!is.na(long), !is.na(lat)) %>% 
   st_as_sf(coords = c("long", "lat"), crs = 4326)
 
+ggplot()+
+  geom_sf(data=grilla_caba_c, aes(color=robo))+
+  geom_sf(data=comisarias, color="red", size=5)
+
+#Distancia centroides delitos a comisarias
+comisarias_grilla <- st_join(grilla_caba_c, comisarias, st_nearest_feature)
+
+#Agregamos una columna con la distancia y lo pasamos a numero
+comisarias_grilla <- comisarias_grilla %>% 
+  mutate(distancia=st_distance(grilla_caba_c, comisarias[st_nearest_feature(grilla_caba_c, comisarias),], by_element = TRUE)) %>% 
+  mutate(distancia=as.numeric(distancia))
+
+modelo<- lm(robos~distancia, data=comisarias_grilla)
+summary(modelo)
 
 
 barrios <- read_sf("http://cdn.buenosaires.gob.ar/datosabiertos/datasets/barrios/barrios.geojson")
